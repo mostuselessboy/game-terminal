b165ae883825a60e0949f3cbd7d2624e60842ec8
mostuselessboy <mostuselessboy@gmail.com>
2025-05-07 14:09:27 +0530

    fixed

--- app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import MyLottieComponent from "../ReimaginedSection/lottie-1";
-
-export default function SamplePage() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-    <MyLottieComponent />
-    </div>
-  )
-}
