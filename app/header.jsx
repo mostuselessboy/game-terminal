456290bfc4d7b865e75babd05dfa874f440b3bb8
mostuselessboy <mostuselessboy@gmail.com>
2025-05-18 15:05:05 +0530

    fix(bug): fixed header

--- app/header.jsx
+++ app/header.jsx
@@ -132,7 +132,7 @@ const Navbar = () => {
             </button>
           </div>
 
-          <div className="mobile-menu-items max-h-[85mvh] overflow-scroll">
+          <div className="mobile-menu-items max-h-[80vh] overflow-scroll">
             {menuItems.map((item) => (
               <Link
                 key={item.path}
